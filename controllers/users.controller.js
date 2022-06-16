9ef49dfc3657680c2f06e1ab55287eba1d197481
JoaoffPinho <joao.f.pinho.com>
2022-06-16 15:45:51 +0100

    del user

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -556,7 +556,7 @@ exports.removeSerieSeen = async (req, res) => {
 
 exports.delete = async (req, res) => {
     try {
-    const user = await User.findOneAndRemove(req.body.name).exec();
+    const user = await User.deleteOne({"name": req.body.name}).exec();
     if (!user) // returns the deleted document (if any) to the callback
     res.status(404).json({
     message: `Not found user with name=${req.body.name}.`
